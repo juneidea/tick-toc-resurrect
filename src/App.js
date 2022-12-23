33de21c5b3bf7e5a6caf600195554c2d97498dfd
Suparoek Taetiangthum <jsuparoek@Suparoeks-MBP.attlocal.net>
2022-12-22 22:47:25 -0600

    start /

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, {Component, Fragment} from 'react'
+import {Switch, Route, withRouter, Redirect} from 'react-router-dom'
+import {connect} from 'react-redux'
 import './App.css';
+import Main from './Main'
+import {me} from './store'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.loadInitialData()
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route render={() => <Redirect to="/" />} />
+        </Switch>
+      </Fragment>
+    )
+  }
 }
 
-export default App;
+const mapState = state => {
+  return {
+    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
+    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
+    isLoggedIn: !!state.user.user.id,
+    isFetching: state.user.isFetching
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    loadInitialData: () => dispatch(me())
+    // fetchWebRTC: () => dispatch(Stream())
+  }
+}
+
+// The `withRouter` wrapper makes sure that updates are not blocked
+// when the url changes
+export default withRouter(connect(mapState, mapDispatch)(App))
---
+++ src/Main.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react'
+import './styles/Main.css'
+import {Link} from 'react-router-dom'
+import {logout, resetGame} from './store'
+import {connect} from 'react-redux'
+
+class Main extends Component {
+  handleClick = (event) => {
+    event.preventDefault()
+    this.props.resetGame()
+    this.props.logout()
+  }
+
+  render() {
+    return (
+      <div className="main-container">
+        <div>
+          <div className="main-options">
+            <Link className="main-option" to="new-game">
+              Start Game
+            </Link>
+            <Link className="main-option" to="previous-games">
+              Previous Games
+            </Link>
+            <Link className="main-option" to="leaderboard">
+              Leaderboard
+            </Link>
+          </div>
+          <Link to="/">
+            <button
+              className="button2 logout-button"
+              onClick={this.handleClick}
+              type="button"
+            >
+              Logout
+            </button>
+          </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+const mapDispatch = dispatch => {
+  return {
+    logout: () => dispatch(logout()),
+    resetGame: () => dispatch(resetGame())
+  }
+}
+
+export default connect(null, mapDispatch)(Main)
