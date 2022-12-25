739ce87aec2f13e3613b8eb73be303f198b2f38a
Suparoek Taetiangthum <jsuparoek@Suparoeks-MBP.attlocal.net>
2022-12-24 21:38:51 -0600

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,43 +1,16 @@
-import React, {Component, Fragment} from 'react'
-import {Switch, Route, withRouter, Redirect} from 'react-router-dom'
-import {connect} from 'react-redux'
-import './App.css';
+import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
 import Main from './Main'
-import {me} from './store'
 
-class App extends Component {
-  componentDidMount() {
-    this.props.loadInitialData()
-  }
-
-  render() {
-    return (
-      <Fragment>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route render={() => <Redirect to="/" />} />
-        </Switch>
-      </Fragment>
-    )
-  }
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route render={() => <Navigate to="/" />} />
+      </Routes>
+    </>
+  )
 }
 
-const mapState = state => {
-  return {
-    // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
-    // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.user.id,
-    isFetching: state.user.isFetching
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    loadInitialData: () => dispatch(me())
-    // fetchWebRTC: () => dispatch(Stream())
-  }
-}
-
-// The `withRouter` wrapper makes sure that updates are not blocked
-// when the url changes
-export default withRouter(connect(mapState, mapDispatch)(App))
+export default App
--- src/Main.js
+++ src/Main.js
@@ -1,51 +1,46 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './styles/Main.css'
 import {Link} from 'react-router-dom'
-import {logout, resetGame} from './store'
-import {connect} from 'react-redux'
 
-class Main extends Component {
-  handleClick = (event) => {
+const Main = () => {
+  const handleClick = (event) => {
     event.preventDefault()
-    this.props.resetGame()
-    this.props.logout()
+    // this.props.resetGame()
+    // this.props.logout()
   }
-
-  render() {
-    return (
-      <div className="main-container">
-        <div>
-          <div className="main-options">
-            <Link className="main-option" to="new-game">
-              Start Game
-            </Link>
-            <Link className="main-option" to="previous-games">
-              Previous Games
-            </Link>
-            <Link className="main-option" to="leaderboard">
-              Leaderboard
-            </Link>
-          </div>
-          <Link to="/">
-            <button
-              className="button2 logout-button"
-              onClick={this.handleClick}
-              type="button"
-            >
-              Logout
-            </button>
+  return (
+    <div className="main-container">
+      <div>
+        <div className="main-options">
+          <Link className="main-option" to="new-game">
+            Start Game
+          </Link>
+          <Link className="main-option" to="previous-games">
+            Previous Games
+          </Link>
+          <Link className="main-option" to="leaderboard">
+            Leaderboard
           </Link>
         </div>
+        <Link to="/">
+          <button
+            className="button2 logout-button"
+            onClick={handleClick}
+            type="button"
+          >
+            Logout
+          </button>
+        </Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
-const mapDispatch = dispatch => {
-  return {
-    logout: () => dispatch(logout()),
-    resetGame: () => dispatch(resetGame())
-  }
-}
+// const mapDispatch = dispatch => {
+//   return {
+//     logout: () => dispatch(logout()),
+//     resetGame: () => dispatch(resetGame())
+//   }
+// }
 
-export default connect(null, mapDispatch)(Main)
+export default Main
